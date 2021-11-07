76be1274a4872f1c3c2a83c1741f5893898951cf
Artieficial <93848752+artieficial@users.noreply.github.com>
2021-11-07 00:16:09 -0700

    Add extra functionality

--- ApplyAccessories.cs
+++ ApplyAccessories.cs
@@ -16,9 +16,18 @@ using VRC.SDK3.Avatars.ScriptableObjects;
 public class ApplyAccessoriesEditor : Editor
 {
     public Transform targetArmature;
+    public VRCAvatarDescriptor targetAvatarDescriptor;
+    public AnimationClip targetFemaleAnimationClip;
+    public bool targetNestArmature;
     public Transform targetRootBone;
-
-    bool showOverrideArmature = false;
+    public VRCExpressionsMenu targetSubMenu;
+    public SkinnedMeshRenderer[] associatedMeshRenderers;
+    public Dictionary<SkinnedMeshRenderer, string[]> associatedBlendShapesMap;
+    public string femaleBlendShapeName = "";
+    bool showOverrideArmature = true;
+    bool showFemaleBlendShapes = true;
+    bool showAssociatedMeshRenderers = true;
+    bool[] showPanelsForAssociatedMeshRenderers = {};
 
     public override void OnInspectorGUI()
     {
@@ -26,20 +35,101 @@ public class ApplyAccessoriesEditor : Editor
         
         ApplyAccessories applyAccessories = (ApplyAccessories)target;
 
+        // Make sure that all the values are loaded from the actual component
+        targetArmature = applyAccessories.getTargetArmature();
+        targetAvatarDescriptor = applyAccessories.getTargetAvatarDescriptor();
+        targetFemaleAnimationClip = applyAccessories.getTargetFemaleAnimationClip();
+        targetNestArmature = applyAccessories.getTargetNestArmature();
+        targetRootBone = applyAccessories.getTargetRootBone();
+        targetSubMenu = applyAccessories.getTargetSubMenu();
+        associatedBlendShapesMap = applyAccessories.getAssociatedBlendShapes();
+        associatedMeshRenderers = applyAccessories.getAssociatedMeshRenderers();
+        femaleBlendShapeName = applyAccessories.getFemaleBlendShapeName();
+
+        // Set up top level settings
+        targetAvatarDescriptor = EditorGUILayout.ObjectField("Target Avatar Descriptor", targetAvatarDescriptor, typeof(VRCAvatarDescriptor), true) as VRCAvatarDescriptor;
+        targetSubMenu = EditorGUILayout.ObjectField("Target Submenu (Optional)", targetSubMenu, typeof(VRCExpressionsMenu), true) as VRCExpressionsMenu;
+        targetNestArmature = EditorGUILayout.Toggle("Nest Armature", targetNestArmature);
+
         // Allow for setting the armature manually
-        showOverrideArmature = EditorGUILayout.BeginFoldoutHeaderGroup(showOverrideArmature, "Override Default Armature");
+        showOverrideArmature = EditorGUILayout.BeginFoldoutHeaderGroup(showOverrideArmature, "Override Default Armature (Optional)");
         if (showOverrideArmature)
         {
-            EditorGUILayout.ObjectField("Target Armature", targetArmature, typeof(Transform), true);
-            EditorGUILayout.ObjectField("Target Root Bone", targetRootBone, typeof(Transform), true);
+            targetArmature = EditorGUILayout.ObjectField("Target Armature", targetArmature, typeof(Transform), true) as Transform;
+            targetRootBone = EditorGUILayout.ObjectField("Target Root Bone", targetRootBone, typeof(Transform), true) as Transform;
         }
         EditorGUILayout.EndFoldoutHeaderGroup();
+
+        // Determine whether or not the accessory include female blend shapes and apply them
+        showFemaleBlendShapes = EditorGUILayout.BeginFoldoutHeaderGroup(showFemaleBlendShapes, "Set Female Blendshapes (Optional)");
+        if (showFemaleBlendShapes)
+        {
+            targetFemaleAnimationClip = EditorGUILayout.ObjectField("Animation Clip", targetFemaleAnimationClip, typeof(AnimationClip), true) as AnimationClip;
+            femaleBlendShapeName = EditorGUILayout.TextField("Blendshape Name", femaleBlendShapeName);
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        // Determine if there are any other mesh renderers that interact with the accessories
+        showAssociatedMeshRenderers = EditorGUILayout.BeginFoldoutHeaderGroup(showAssociatedMeshRenderers, "Set Associated Mesh Renderers (Optional)");
+        if (showAssociatedMeshRenderers)
+        {
+            int numAssociatedMeshRenderers = EditorGUILayout.IntField("Length", associatedMeshRenderers.Length);
+            if (numAssociatedMeshRenderers != associatedMeshRenderers.Length)
+            {
+                Array.Resize(ref associatedMeshRenderers, numAssociatedMeshRenderers);
+                
+            }
+            
+            for (int i = 0; i < numAssociatedMeshRenderers; i++)
+            {
+                associatedMeshRenderers[i] = EditorGUILayout.ObjectField($"Mesh Renderer #{i + 1}", associatedMeshRenderers[i], typeof(SkinnedMeshRenderer), true) as SkinnedMeshRenderer;
+                if (associatedMeshRenderers[i] != null && !associatedBlendShapesMap.ContainsKey(associatedMeshRenderers[i]))
+                {
+                    string[] blendShapes = {};
+                    associatedBlendShapesMap[associatedMeshRenderers[i]] = blendShapes;
+                }
+            }
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        if (associatedMeshRenderers.Length != showPanelsForAssociatedMeshRenderers.Length)
+        {
+            Array.Resize(ref showPanelsForAssociatedMeshRenderers, associatedMeshRenderers.Length);
+        }
+
+        for (int i = 0; i < associatedMeshRenderers.Length; i++)
+        {
+            if (associatedMeshRenderers[i] != null && associatedBlendShapesMap.ContainsKey(associatedMeshRenderers[i]))
+            {
+                showPanelsForAssociatedMeshRenderers[i] = EditorGUILayout.BeginFoldoutHeaderGroup(showPanelsForAssociatedMeshRenderers[i], $"Blendshapes for Mesh Renderer #{i + 1}");
+                string[] associatedBlendShapes = associatedBlendShapesMap[associatedMeshRenderers[i]];
+                int numAssociatedBlendShapes = EditorGUILayout.IntField("Length", associatedBlendShapes.Length);
+                Array.Resize(ref associatedBlendShapes, numAssociatedBlendShapes);
+
+                for (int j = 0; j < numAssociatedBlendShapes; j++)
+                {
+                    associatedBlendShapes[j] = EditorGUILayout.TextField($"Blendshape #{j + 1}", associatedBlendShapes[j]);
+                } 
+
+                associatedBlendShapesMap[associatedMeshRenderers[i]] = associatedBlendShapes;
+                EditorGUILayout.EndFoldoutHeaderGroup();
+            }
+        }
+
+        // Apply everything back to the main class
+        applyAccessories.setTargetArmature(targetArmature);
+        applyAccessories.setTargetAvatarDescriptor(targetAvatarDescriptor);
+        applyAccessories.setTargetFemaleAnimationClip(targetFemaleAnimationClip);
+        applyAccessories.setTargetNestArmature(targetNestArmature);
+        applyAccessories.setTargetRootBone(targetRootBone);
+        applyAccessories.setTargetSubMenu(targetSubMenu);
+        applyAccessories.setAssociatedBlendShapes(associatedBlendShapesMap);
+        applyAccessories.setAssociatedMeshRenderers(associatedMeshRenderers);
+        applyAccessories.setFemaleBlendShapeName(femaleBlendShapeName);
 
         // Apply actual changes
         if (GUILayout.Button("Apply Changes"))
         {
-            applyAccessories.setTargetArmature(targetArmature);
-            applyAccessories.setTargetRootBone(targetRootBone);
             applyAccessories.Reassign();
         }
     }
@@ -48,16 +138,71 @@ public class ApplyAccessoriesEditor : Editor
 [ExecuteInEditMode]
 public class ApplyAccessories : MonoBehaviour
 {
-    public VRCAvatarDescriptor targetAvatarDescriptor;
-    public VRCExpressionsMenu targetSubMenu;
-
     private Transform targetArmature;
-    private Transform targetRootBone;
+    private VRCAvatarDescriptor targetAvatarDescriptor;
     private VRCExpressionParameters targetAvatarParameters;
+    private AnimationClip targetFemaleAnimationClip;
+    private bool targetNestArmature;
+    private Transform targetRootBone;
+    private VRCExpressionsMenu targetSubMenu;
+    private Dictionary<SkinnedMeshRenderer, string[]> associatedBlendShapes = new Dictionary<SkinnedMeshRenderer, string[]>();
+    private SkinnedMeshRenderer[] associatedMeshRenderers = {};
+   
+    private string femaleBlendShapeName;
+
+    public VRCAvatarDescriptor getTargetAvatarDescriptor()
+    {
+        return targetAvatarDescriptor;
+    }
+
+    public void setTargetAvatarDescriptor(VRCAvatarDescriptor target)
+    {
+        targetAvatarDescriptor = target;
+    }
+
+    public bool getTargetNestArmature()
+    {
+        return targetNestArmature;
+    }
+
+    public void setTargetNestArmature(bool target)
+    {
+        targetNestArmature = target;
+    }
+
+    public Dictionary<SkinnedMeshRenderer, string[]> getAssociatedBlendShapes()
+    {
+        return associatedBlendShapes;
+    }
+
+    public void setAssociatedBlendShapes(Dictionary<SkinnedMeshRenderer, string[]> blendShapes)
+    {
+        associatedBlendShapes = blendShapes;
+    }
+
+    public SkinnedMeshRenderer[] getAssociatedMeshRenderers()
+    {
+        return associatedMeshRenderers;
+    }
+
+    public void setAssociatedMeshRenderers(SkinnedMeshRenderer[] meshRenderers)
+    {
+        associatedMeshRenderers = meshRenderers;
+    }
+
+    public Transform getTargetArmature()
+    {
+        return targetArmature;
+    }
 
     public void setTargetArmature(Transform target)
     {
         targetArmature = target;
+    }
+
+    public Transform getTargetRootBone()
+    {
+        return targetRootBone;
     }
 
     public void setTargetRootBone(Transform target)
@@ -65,9 +210,34 @@ public class ApplyAccessories : MonoBehaviour
         targetRootBone = target;
     }
 
+    public VRCExpressionsMenu getTargetSubMenu()
+    {
+        return targetSubMenu;
+    }
+
     public void setTargetSubMenu(VRCExpressionsMenu target)
     {
         targetSubMenu = target;
+    }
+
+    public AnimationClip getTargetFemaleAnimationClip()
+    {
+        return targetFemaleAnimationClip;
+    }
+
+    public void setTargetFemaleAnimationClip(AnimationClip target)
+    {
+        targetFemaleAnimationClip = target;
+    }
+
+    public string getFemaleBlendShapeName()
+    {
+        return femaleBlendShapeName;
+    }
+
+    public void setFemaleBlendShapeName(string target)
+    {
+        femaleBlendShapeName = target;
     }
 
     AnimatorController getFXController()
@@ -88,12 +258,22 @@ public class ApplyAccessories : MonoBehaviour
     
         if (!animationClip) {
             animationClip = new AnimationClip();
-            animationClip.SetCurve(targetName, typeof(GameObject), "isActive", animationCurve);
+            animationClip.SetCurve(targetName, typeof(GameObject), "m_IsActive", animationCurve);
             AssetDatabase.CreateAsset(animationClip, $"Assets/GeneratedAnimations/{targetName}.anim");
         }
         else
         {
-            animationClip.SetCurve(targetName, typeof(GameObject), "isActive", animationCurve);
+            animationClip.SetCurve(targetName, typeof(GameObject), "m_IsActive", animationCurve);
+        }
+
+        AnimationCurve blendShapeCurve = new AnimationCurve(new Keyframe(0.0f, 100.0f));
+        foreach(SkinnedMeshRenderer renderer in associatedMeshRenderers)
+        {
+            string[] blendShapes = associatedBlendShapes[renderer];
+            foreach(string blendShape in blendShapes)
+            {
+                animationClip.SetCurve(renderer.gameObject.name, typeof(SkinnedMeshRenderer), $"blendShape.{blendShape}", blendShapeCurve);
+            }
         }
 
         AssetDatabase.SaveAssets();
@@ -104,16 +284,23 @@ public class ApplyAccessories : MonoBehaviour
     void updateAnimatorController(string targetName)
     {
         AnimatorController animatorController = getFXController();
+        AnimatorControllerLayer animatorControllerLayer;
 
-        animatorController.AddParameter(targetName, AnimatorControllerParameterType.Bool);
-        AnimatorControllerLayer layer = new AnimatorControllerLayer {
+        // If the parameter already exists, then we already created everything and this step is unnecessary.
+        if (Array.Find(animatorController.parameters, (parameter) => { return parameter.name == targetName; } ) == null)
+            animatorController.AddParameter(targetName, AnimatorControllerParameterType.Bool);
+        else
+            return;
+
+        animatorControllerLayer = new AnimatorControllerLayer {
             defaultWeight = 1.0f,
             name = targetName,
             stateMachine = new AnimatorStateMachine()
         };
-        animatorController.AddLayer(layer);
+        animatorController.AddLayer(animatorControllerLayer);
+        
 
-        AnimatorStateMachine stateMachine = layer.stateMachine;
+        AnimatorStateMachine stateMachine = animatorControllerLayer.stateMachine;
         AnimatorState wait = stateMachine.AddState("Wait");
         AnimatorState target = stateMachine.AddState(targetName);
 
@@ -226,6 +413,14 @@ public class ApplyAccessories : MonoBehaviour
         targetSubMenu.controls.Add(targetControl);
     }
 
+    void updateFemaleAnimationClip(string targetName)
+    {
+        AnimationCurve animationCurve = new AnimationCurve(new Keyframe(0.0f, 100.0f));
+        targetFemaleAnimationClip.SetCurve(targetName, typeof(SkinnedMeshRenderer), $"blendShape.{femaleBlendShapeName}", animationCurve);
+
+        AssetDatabase.SaveAssets();
+    }
+
     [ContextMenu("Reassign Bones")]
     public void Reassign()
     {
@@ -312,11 +507,33 @@ public class ApplyAccessories : MonoBehaviour
 
             for (int s = 0; s < sourceBones.Length; s++)
             {
+                bool matched = false;
                 for (int t = 0; t < targetBones.Length; t++)
                 {
                     if (sourceBones[s].name.Contains(targetBones[t].name)) {
-                        sourceBones[s] = targetBones[t];
+                        Component[] components = sourceBones[s].gameObject.GetComponents<Component>();
+                        if ((components.Length > 1 || targetNestArmature) && sourceBones[s].name != targetBones[t].name)
+                        {
+                            sourceBones[s].parent = targetBones[t];
+                        }
+                        else
+                        {
+                            sourceBones[s] = targetBones[t];
+                        }
+                        
                         break;
+                    }
+                }
+                if (!matched)
+                {
+                    string parentName = sourceBones[s].parent.name;
+                    for (int t = 0; t < targetBones.Length; t++)
+                    {
+                        if (parentName.Contains(targetBones[t].name))
+                        {
+                            sourceBones[s].parent = targetBones[t];
+                            break;
+                        }
                     }
                 }
             }
@@ -329,6 +546,9 @@ public class ApplyAccessories : MonoBehaviour
                 addParameterToExpressions(meshRenderer.name, meshRenderers.Length);
                 addParameterToSubmenu(meshRenderer.name, meshRenderers.Length);
                 updateAnimatorController(meshRenderer.name);
+                if (targetFemaleAnimationClip != null)
+                    updateFemaleAnimationClip(meshRenderer.name);
+                meshRenderer.gameObject.SetActive(false);
             }
 
             meshRenderer.gameObject.transform.parent = targetAvatarDescriptor.gameObject.transform;
